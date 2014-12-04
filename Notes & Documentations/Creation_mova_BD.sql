d319a2d14325d46fc19f55bc60937f2edd330136
AlexGodard <alexandre.godard@hotmail.com>
2014-12-04 14:02:45 -0500

    gfsd

--- Notes & Documentations/Creation_mova_BD.sql
+++ Notes & Documentations/Creation_mova_BD.sql
@@ -150,7 +150,7 @@ CREATE TABLE IF NOT EXISTS `vetements` (
   `idTypeVetement` int(11) NOT NULL,
   `idCouleur` int(11) NOT NULL,
   `nomVetement` varchar(100) NOT NULL,
-  `imageURL` varchar(100) NOT NULL,
+  `imageURL` varchar(255) NOT NULL,
   `prix` float DEFAULT NULL,
   `estHomme` tinyint(1) NOT NULL,
   `estFemme` tinyint(1) NOT NULL,
